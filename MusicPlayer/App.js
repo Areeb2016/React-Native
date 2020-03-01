b164670e48e33e78cec2ad196f0396c42e4dc50b
BitesDigital <BitesDigital@users.noreply.github.com>
2020-03-01 22:31:51 +0500

    Seventh Commit

--- MusicPlayer/App.js
+++ MusicPlayer/App.js
@@ -1,15 +1,37 @@
 import TrackPlayer from 'react-native-track-player';
+import { View, ProgressBarAndroid, Button } from 'react-native';
+import React, { useState, Component } from 'react';
+import { Text } from 'react-native';
+import { useTrackPlayerEvent, TrackPlayerEvents, STATE_PLAYING, useTrackPlayerProgress } from 'react-native-track-player';
+import MusicFiles from 'react-native-get-music-files';
 
+export default class App extends Component{
+
+  componentDidMount(){
 TrackPlayer.setupPlayer().then(async() => {
-    // The player is ready to be used
-    await TrackPlayer.add({
-        id: 'trackId',
-        url: require('./Falling.mp3'),
-        title: 'Falling',
-        artist: 'Trevor Daniel',
-        artwork: require('./falling.jpg')
-    });
- 
-    // Starts playing it
-    TrackPlayer.play();
+  // The player is ready to be used
+  await TrackPlayer.add({
+      id: 'trackId',
+      url: require('./Falling.mp3'),
+      title: 'Falling',
+      artist: 'Trevor Daniel',
+      artwork: require('./falling.jpg')
+  });
+
+
 });
+
+
+}
+
+render(){
+
+
+//In order to get blocks of songs, for fix performance issues at least in Android, use next
+return(
+  <View>
+    <Button title="Play" onPress={this.TrackPlayer.play()}></Button>
+  </View>
+)
+}
+}
